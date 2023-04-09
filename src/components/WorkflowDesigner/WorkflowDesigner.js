dbfabb49ceeec28439381b082894fed15d6f5e87
Anshika <anshika2011087@akgec.ac.in>
2023-04-09 16:21:22 +0530

    movable node with 0 content

--- src/components/WorkflowDesigner/WorkflowDesigner.js
+++ src/components/WorkflowDesigner/WorkflowDesigner.js
@@ -1,11 +1,109 @@
+// import React, { useEffect, useState } from 'react';
+// import ReactFlow, { addEdge, removeElements } from 'react-flow-renderer';
+
+
+
+// const WorkflowDesigner = ({ match }) => {
+//   const [workflowData, setWorkflowData] = useState(null);
+//   const [modules, setModules] = useState([]);
+//   const [elements, setElements] = useState([]);
+
+//   useEffect(() => {
+//     // Load workflow data and modules on mount
+//     fetch(`https://64307b10d4518cfb0e50e555.mockapi.io/workflow/${match.params.id}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setWorkflowData(data);
+//         setElements([
+//           {
+//             id: 'input',
+//             type: 'input',
+//             data: {
+//               label: data.input,
+//             },
+//             position: { x: 0, y: 50 },
+//           },
+//         ]);
+//       });
+
+//     fetch(`https://64307b10d4518cfb0e50e555.mockapi.io/modules?page=1&limit=5`)
+//       .then((response) => response.json())
+//       .then((data) => setModules(data));
+//   }, [match.params.id]);
+
+//   const handleDragOver = (event) => {
+//     event.preventDefault();
+//     event.dataTransfer.dropEffect = 'move';
+//   };
+
+//   const handleDrop = (event) => {
+//     event.preventDefault();
+//     const module = JSON.parse(event.dataTransfer.getData('application/module'));
+//     const position = {
+//       x: event.clientX - event.target.getBoundingClientRect().left,
+//       y: event.clientY - event.target.getBoundingClientRect().top,
+//     };
+//     const element = {
+//       id: module.id,
+//       data: { label: module.name },
+//       type: 'default',
+//       position,
+//     };
+//     setElements((e) => addEdge({ ...element, source: 'input', target: element.id }, e));
+//   };
+
+//   const handleDelete = (event, element) => {
+//     event.preventDefault();
+//     setElements((e) => removeElements([element], e));
+//   };
+
+//   const handleValidate = (event, element) => {
+//     event.preventDefault();
+//     const valid = elements.some((e) => e.target === element.id);
+//     setElements((e) =>
+//       e.map((el) => {
+//         if (el.id === element.id) {
+//           el.style = {
+//             border: `2px solid ${valid ? 'green' : 'red'}`,
+//             ...el.style,
+//           };
+//         }
+//         return el;
+//       })
+//     );
+//   };
+
+//   if (!workflowData) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div onDragOver={handleDragOver} onDrop={handleDrop}>
+//       <h1>{workflowData.name}</h1>
+//       <ReactFlow elements={elements} onElementsRemove={handleDelete} onNodeDragStop={handleValidate} />
+//       <div>
+//         {modules.map((module) => (
+//           <div key={module.id} draggable onDragStart={(event) => event.dataTransfer.setData('application/module', JSON.stringify(module))}>
+//             {module.name}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WorkflowDesigner;
+
 import React, { useCallback,useEffect, useState } from 'react';
-import ReactFlow, {  addEdge, applyEdgeChanges, applyNodeChanges } from 'react-flow-renderer';
+import ReactFlow, { addEdge, applyNodeChanges  } from 'react-flow-renderer';
+
+
 
 const WorkflowDesigner = ({ match }) => {
   const [workflowData, setWorkflowData] = useState(null);
   const [modules, setModules] = useState([]);
+  // const [elements, setElements] = useState([]);
   const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState([]);
 
   useEffect(() => {
     // Load workflow data and modules on mount
@@ -25,7 +123,7 @@ const WorkflowDesigner = ({ match }) => {
         ]);
       });
 
-    fetch(`https://64307b10d4518cfb0e50e555.mockapi.io/workflow?page=1&limit=5`)
+    fetch(`https://64307b10d4518cfb0e50e555.mockapi.io/modules?page=1&limit=5`)
       .then((response) => response.json())
       .then((data) => setModules(data));
   }, [match.params.id]);
@@ -34,6 +132,11 @@ const WorkflowDesigner = ({ match }) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'move';
   };
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
 
   const handleDrop = (event) => {
     event.preventDefault();
@@ -48,64 +151,25 @@ const WorkflowDesigner = ({ match }) => {
       type: 'default',
       position,
     };
-    setNodes((n) => [...n, node]);
-    setEdges((e) => [
-      ...e,
-      {
-        id: `e${nodes.length}-${module.id}`,
-        source: 'input',
-        target: module.id,
-        animated: true,
-      },
-    ]);
+    setNodes((e) => addEdge({ ...node, source: 'input', target: node.id }, e));
   };
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes]
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges]
-  );
-  const onConnect = useCallback(
-    (connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges]
-  );
-  // const handleDelete = (event, element) => {
-  //   event.preventDefault();
-  //   if (element.type === 'node') {
-  //     setNodes((n) => n.filter((node) => node.id !== element.id));
-  //     setEdges((e) => e.filter((edge) => edge.source !== element.id && edge.target !== element.id));
-  //   } else {
-  //     setEdges((e) => e.filter((edge) => edge.id !== element.id));
-  //   }
-  // };
 
-  const handleValidate = (event, element) => {
+ 
+
+  const handleValidate = (event, node) => {
     event.preventDefault();
-    const valid = edges.some((e) => e.target === element.id);
-    if (element.type === 'node') {
-      setNodes((n) =>
-        n.map((node) => {
-          if (node.id === element.id) {
-            node.style = {
-              border: `2px solid ${valid ? 'green' : 'red'}`,
-              ...node.style,
-            };
-          }
-          return node;
-        })
-      );
-    } else {
-      setEdges((e) =>
-        e.map((edge) => {
-          if (edge.id === element.id) {
-            edge.animated = valid;
-          }
-          return edge;
-        })
-      );
-    }
+    const valid = nodes.some((e) => e.target === node.id);
+    setNodes((e) =>
+      e.map((el) => {
+        if (el.id === node.id) {
+          el.style = {
+            border: `2px solid ${valid ? 'green' : 'red'}`,
+            ...el.style,
+          };
+        }
+        return el;
+      })
+    );
   };
 
   if (!workflowData) {
@@ -113,19 +177,16 @@ const WorkflowDesigner = ({ match }) => {
   }
 
   return (
-    <div onDragOver={handleDragOver} onDrop={handleDrop}>
+    <div style={{ width: '100vw', height: '100vh' }} onDragOver={handleDragOver} onDrop={handleDrop}>
       <h1>{workflowData.name}</h1>
-      <ReactFlow elements={[...nodes, ...edges]}  nodes={nodes}
-      edges={edges} onConnect={onConnect} onEdgesChange={onEdgesChange} onNodesChange={onNodesChange} onNodeDragStop={handleValidate}/>
-
-
-
+      <ReactFlow  nodes={nodes} onNodesChange={onNodesChange} onNodeDragStop={handleValidate} />
+      <div>
         {modules.map((module) => (
           <div key={module.id} draggable onDragStart={(event) => event.dataTransfer.setData('application/module', JSON.stringify(module))}>
             {module.name}
           </div>
         ))}
-   
+      </div>
     </div>
   );
 };
